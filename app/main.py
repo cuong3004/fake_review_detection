94ead1ce20ec11455498416f24b71b4d0a50922b
cuong3004 <duongduccuong30042002@gmail.com>
2024-02-19 11:26:29 +0700

    update

--- app/main.py
+++ app/main.py
@@ -2,13 +2,13 @@ from tkinter import *
 from tkinter.ttk import *
 import subprocess
 from process_data import ProcessData
+import time 
 
 class Test():
     def __init__(self):
 
         self.processData = ProcessData()
 
-        # print(self.processData )
         self.root = Tk()
         self.root.geometry('300x250')
         self.button = Button(self.root,
@@ -17,7 +17,13 @@ class Test():
 
         self.button.pack()
 
-          
+        self.button2 = Button(self.root,
+                          text = 'Predict',
+                          command=lambda:self._predict())
+
+        self.button2.place(x = 111,y = 50)
+        self.e2 = Entry(self.root, width = 16)
+        self.e2.place(x = 100, y = 75)
 
         self.pb1 = Progressbar(self.root, orient=HORIZONTAL, length=100, mode='indeterminate')
         self.pb1.pack(expand=True)
@@ -25,8 +31,12 @@ class Test():
         self.root.mainloop()            
         self.text_entry = str()
 
-
-
+    def _predict(self):
+        text = self.e2.get()
+        label = self.processData.predict(text)
+        label_dict = {0:"Fake", 1:"Trust"}
+        lb = Label(self.root, text=label_dict[label])
+        lb.place(x = 200, y = 75)
 
     def set_e(self, text):
 
@@ -37,9 +47,11 @@ class Test():
     def scrape(self):
         # pass
         # print(self.e.get())
+        time1 = time.time()
         process = subprocess.Popen(f"scrapy runspider Scrape_AmazonReviews/Scrape_AmazonReviews/spiders/AmazonReviews.py -o output.csv -a myBaseUrl={self.text_entry}")
         process.wait()
         true_lie_dir, len_data = self.processData.process_data()
+
 
         self.lb1 = Label(self.root, 
                       text = f"Trust: {str(true_lie_dir[1])}").place(x = 40,
@@ -51,8 +63,10 @@ class Test():
         self.lb3 = Label(self.root, 
                       text = f"Total: {len_data}").place(x = 40,
                                                y = 150)
-
-        pass
+        time_delta = time.time() - time1
+        self.lb4 = Label(self.root, 
+                      text = f"Time: {int(time_delta)}s").place(x = 40,
+                                               y = 170)
 
     def pop_up(self):
         top = Toplevel()
@@ -63,10 +77,10 @@ class Test():
         scrape = Button(top, text="Start", command=lambda: [self.set_e(e.get()), top.destroy(), self.scrape()])
 
         # btn.grid(row=0, column=0)
-        btn.place(x = 5, y = 10)           
+        btn.place(x = 5, y = 5)           
         # e.grid(row=1, column=100)
-        e.place(x = 5, y = 40)
-        scrape.place(x = 5, y = 68)
+        e.place(x = 80, y = 30)
+        scrape.place(x = 5, y = 50)
         # scrape.grid(row=2, column=0)
 
 
